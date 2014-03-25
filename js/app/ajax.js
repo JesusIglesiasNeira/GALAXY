8915fb2003dbafafa8cb1ab0e68930b92488d8c4
JesusIglesiasNeira <jesusigne@gmail.com>
2014-03-25 22:58:56 +0100

    javascrits

--- js/app/ajax.js
+++ js/app/ajax.js
@@ -13,18 +13,27 @@ App.ajax = (function(){
                 data : { user : usuario },
                 dataType : 'json',
                 cache : false,
-                success : crearLista,
+                success : control.crearLista,
                 error : function(jqXHR, textStatus, errorThrow){
                     alert(errorThrow);
                     console.log(errorThrow);
                 }
             });
-
     };
 
     var envDatos = function (datos){
-
-
+        $.ajax({
+                url : '../servidor/enviaResultadosJSON.php',
+                type : 'POST',
+                data : { ordenesCompletadas : datos },
+                dataType : 'json',
+                cache : false,
+                success : control.ordenesEnviadas,
+                error : function(jqXHR, textStatus, errorThrow){
+                    alert(errorThrow);
+                    console.log(errorThrow);
+                }
+        });
     };
 
 
---
+++ js/app/control.js
@@ -0,0 +1,88 @@
+﻿var App = App||{};
+App.control = (function(){
+    var codUsu;
+    var liTareas;
+///////////Func a realizar cuando se genere el evento////////////
+    var actualizar = function(){
+        ajax.obtenerOrdenes(codUsu);
+        inicial();
+    };
+
+    var enviarDatos = function(tareasRealizadas){
+        ajax.enviarDatos(tareasRealizadas);
+    };
+
+    var guardar = function(datos){
+        data.guardarCambios(datos);
+    };
+
+
+
+
+
+
+
+//////////////////////////////AJAX//////////////////////////////
+    var crearLista = function(datos){
+        data.guardarCambios(datos);
+    };
+
+    var ordenesEnviadas = function(){
+        alert('Ordenes enviadas Correctamente');
+    };
+
+
+
+
+
+
+
+/////////////////////////////Vistas/////////////////////////////
+
+    var mostrarConfig = function(){
+        vistas.mostrarPantallaConfiguracion();
+    };
+
+    var mostrarListaTareas = function(){
+         vistas.mostrarPantallaListaTareas(liTareas);
+    };
+
+    var mostrarTarea = function(){
+         vistas.mostrarTarea(/**/);
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+    ////Función inicial que se ejecuta al cargar control.js//////
+
+    function inicial(){
+        codUsu = data.obtenerCodUsu();
+        if (codUsu && codUsu.length > 0){
+            liTareas = data.obtenerListaTareas();    //locales
+            if (liTareas && liTareas.length > 0 ){
+                vistas.mostrarPantallaListaTareas(liTareas);
+            }
+        }
+        else {alert('Codigo de usuario incorrecto');}
+    }
+
+
+
+
+
+        return{
+        crearLista : crearLista
+    };
+
+
+
+});
