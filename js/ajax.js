c58208b5b74de8f8969ac87d6c0e3339f4364b67
Jesus <daw@daw-VirtualBox.(none)>
2014-03-25 19:22:51 +0100

    ajax

---
+++ js/ajax.js
@@ -0,0 +1,40 @@
+﻿var App = App||{};
+App.ajax = (function(){
+
+    var crearLista = function(data){
+        return data;
+    };
+
+
+    var obtOrdenes = function(usuario){
+        $.ajax({
+                url : '../servidor/cargaOrdenesJSON.php',
+                type : 'POST',
+                data : { user : usuario },
+                dataType : 'json',
+                cache : false,
+                success : crearLista,
+                error : function(jqXHR, textStatus, errorThrow){
+                    alert(errorThrow);
+                    console.log(errorThrow);
+                }
+            });
+
+    };
+
+    var envDatos = function (datos){
+
+
+    };
+
+
+
+
+
+
+    return{
+        obtenerOrdenes : obtOrdenes,
+        enviarDatos : envDatos
+    };
+
+});
